7c2beee3e3872776d1df9661ef3ecc1733f2add5
Tharunkumar k <84740138+tharunkathir2001@users.noreply.github.com>
2025-01-26 13:37:35 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  env: {
+      NEXT_TELEMETRY_DISABLED: '1',
+    },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+
 };
 
 export default nextConfig;
